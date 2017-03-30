e6928bb75db8909d570223a7471ecb193873e068
Arnelle Balane <arnellebalane@gmail.com>
2017-03-30 19:11:21 +0800

    update event venue and map

--- static/javascripts/sw.js
+++ static/javascripts/sw.js
@@ -2,7 +2,7 @@ importScripts('/offline-google-analytics/offline-google-analytics-import.js');
 goog.offlineGoogleAnalytics.initialize();
 
 
-const cacheName = 'wtm17-v6';
+const cacheName = 'wtm17-v7';
 const pathsToCache = [
   '/',
   '/coc',
